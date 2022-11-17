98d64b5b3c12c42304628da99fdbe34822ea2697
Sumit Pandey <94867209+sqrt676@users.noreply.github.com>
2022-11-17 05:57:46 +0530

    Update stgitf2.py

--- stgitf2.py
+++ stgitf2.py
@@ -43,7 +43,7 @@ if (st.button("Click to Summarize",key="sum")):
     for word in nltk.word_tokenize(formatted_article_text):
         if word not in stopwords:
             if word not in word_frequencies.keys():
-            word_frequencies[word] = 1
+                word_frequencies[word] = 1
         else:
             word_frequencies[word] += 1
     maximum_frequncy = max(word_frequencies.values())
