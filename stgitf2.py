b28810102a98dcc6e228d06e16939f79f5c006c2
Sumit Pandey <94867209+sqrt676@users.noreply.github.com>
2022-11-17 05:55:47 +0530

    Update stgitf2.py

--- stgitf2.py
+++ stgitf2.py
@@ -17,91 +17,64 @@ wl=st.text_input("enter the wikepedia link you want to get summary for")
 LoS = st.slider('In How many lines you want the summary?', 1, 30, 25)
 
 
-scraped_data = urllib.request.urlopen(wl)
-article = scraped_data.read()
+if (st.button("Click to Summarize",key="sum")):
+    scraped_data = urllib.request.urlopen(wl)
+    article = scraped_data.read()
 
-parsed_article = bs.BeautifulSoup(article,'lxml')
+    parsed_article = bs.BeautifulSoup(article,'lxml')
 
-paragraphs = parsed_article.find_all('p')
+    paragraphs = parsed_article.find_all('p')
 
-article_text = ""
+    article_text = ""
 
-for p in paragraphs:
-    article_text += p.text
+    for p in paragraphs:
+        article_text += p.text
+    print(len(article_text))
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+    article_text = re.sub(r'\s+', ' ', article_text)
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    #print(len(formatted_article_text))
+    sentence_list = nltk.sent_tokenize(article_text)
 
+    stopwords = nltk.corpus.stopwords.words('french')
 
-
-
-
-print(len(article_text))
-article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-article_text = re.sub(r'\s+', ' ', article_text)
-
-
-
-
-
-formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
-formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
-
-
-
-
-
-#print(len(formatted_article_text))
-sentence_list = nltk.sent_tokenize(article_text)
-
-
-
-
-stopwords = nltk.corpus.stopwords.words('french')
-
-word_frequencies = {}
-for word in nltk.word_tokenize(formatted_article_text):
-    if word not in stopwords:
-        if word not in word_frequencies.keys():
+    word_frequencies = {}
+    for word in nltk.word_tokenize(formatted_article_text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
             word_frequencies[word] = 1
         else:
             word_frequencies[word] += 1
+    maximum_frequncy = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
 
 
 
-maximum_frequncy = max(word_frequencies.values())
-
-for word in word_frequencies.keys():
-    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
-
-
-
-sentence_scores = {}
-for sent in sentence_list:
-    for word in nltk.word_tokenize(sent.lower()):
-        if word in word_frequencies.keys():
-            if len(sent.split(' ')) < 30:
-                if sent not in sentence_scores.keys():
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
                     sentence_scores[sent] = word_frequencies[word]
                 else:
                     sentence_scores[sent] += word_frequencies[word]
 
+    import heapq
+    summary_sentences = heapq.nlargest(LoS, sentence_scores, key=sentence_scores.get)
 
+    summary = ' '.join(summary_sentences)
 
+    #print(summary)
+    st.write(summary)
+    st.subheader("If the article is in other language, see below in english!!")
 
+    from googletrans import Translator
 
-import heapq
-summary_sentences = heapq.nlargest(LoS, sentence_scores, key=sentence_scores.get)
+    translator = Translator()
 
-summary = ' '.join(summary_sentences)
-
-#print(summary)
-st.write(summary)
-
-
-
-st.subheader("If the article is in other language, see below in english!!")
-
-from googletrans import Translator
-
-translator = Translator()
-
-translated_text = translator.translate(summary)
-st.write(translated_text.text)
+    translated_text = translator.translate(summary)
+    st.write(translated_text.text)
